863804873fa0abe83111cd193330cc6f7ecf95b0
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-04 18:28:56 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,13 +6,16 @@ def subtraction(num1 = 10, num2 = 5)
   num1 - num2
 end
 
-def division(num1, num2)
+def division(num1 = 50, num2 = 2)
+  num1 / num2
 end
 
-def multiplication(num1, num2)
+def multiplication(num1 = 4, num2 = 30)
+  num1 * num2
 end
 
 def modulo(num1, num2)
+  
 end
 
 def square_root(num)
